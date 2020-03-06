42cc21f18113a9e62e067c439a97ba451a359d79
Chase White <chase7white14@gmail.com>
2020-03-06 15:58:38 -0700

    DevmountainNullPage

---
+++ pageObjects/DevmountnainNullPage.js
@@ -0,0 +1,23 @@
+module.exports= {
+    url:"https://devmountain-qa.github.io/Automation-Basics/build/",
+    elements: {
+        // seatchbar:(''),
+        evenOddButton:('button[name="evenOddButton"]'),
+        objectFilterButton:('button[name="objectFilterButton"]'),
+        nameFilterButton:('button[id="nameFilterButton"]'),
+        palindromeButton:('button[name="palindromeButton"]'),
+        sumButton:('button[name="sumButton"]'),
+        evenOddInput:('input[name="evenOddInput"]'),
+        objectFilterInput:('input[name="objectFilterInput"]'),
+        nameFilterInput:('input[id="nameFilterInput"]'),
+        palindromeInput:('input[name="palindromeInput"]'),
+        sumInput1:('input[name="sumInput1"]'),
+        sumInput2:('input[name="sumInput2"]'),
+        evenResults:('span[name="evenResults"]'),
+        oddResults:('span[name="oddResults"]'),
+        objectFilterResults:('span[name="objectFilterResults"]'),
+        nameFilterResults:('span[name="nameFilterResults"]'),
+        palindromeResults:('span[name="palindromeResults"]'),
+        sumResults:('span[name="sumResults"]')
+    },
+}
