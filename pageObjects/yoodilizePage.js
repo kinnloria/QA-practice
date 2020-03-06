cc00ff3bd2cc28cc26b3e1df39c0ba2692eff9a2
Chase White <chase7white14@gmail.com>
2020-03-06 09:32:55 -0700

    YoodilizePage test

---
+++ pageObjects/yoodilizePage.js
@@ -0,0 +1,75 @@
+module.exports ={
+    url:"https://www.yoodlize.com/",
+    elements:{
+        searchbar:'input[placeholder="Search for an item"]',
+        results:'button[class="sc-uJMKN fpBmEu sc-ifAKCX kvYMhQ"]',
+        home:'img[src="/images/logo/blueRaw.png"]',
+        check:{
+            selector: '//div[@class="sc-jKVCRD jSqgxr"]',
+            locateStrategy: 'xpath'
+        },
+        recvehicles: {
+            selector: ('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[1]'),
+            locateStrategy: 'xpath'
+        },
+        OutdoorGear:{
+            selector:('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[2]'),
+            locateStrategy:('xpath')
+        },
+        Electronics:{
+            selector:('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[3]'),
+            locateStrategy:('xpath')
+        },
+        party: {
+            selector:('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[4]'),
+            locateStrategy:('xpath')
+        },
+        tools: {
+            selector:('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[5]'),
+            locateStrategy:('xpath')
+        },
+        lotsofcotton: {
+            selector:('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[6]'),
+            locateStrategy:('xpath')
+        },
+        homeandk: {
+            selector:('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[7]'),
+            locateStrategy:('xpath')
+        },
+        gamermode: {
+            selector:('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[8]'),
+            locateStrategy:('xpath')
+        },
+        lawnandgarden: {
+            selector:('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[9]'),
+            locateStrategy:('xpath')
+        },
+        sportingGoods: {
+            selector:('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[10]'),
+            locateStrategy:('xpath')
+        },
+        dvds: {
+            selector:('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[11]'),
+            locateStrategy:('xpath')
+        },
+        venues: {
+            selector:('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[12]'),
+            locateStrategy:('xpath')
+        },
+        music: {
+            selector:('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[13]'),
+            locateStrategy:('xpath')
+        },
+        businessequipment: {
+            selector:('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[14]'),
+            locateStrategy:('xpath')
+        },
+        misc: {
+            selector:('(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[15]'),
+            locateStrategy:('xpath')
+        },
+
+
+
+    }
+}
